b8e6550d14106f5852b769470e1935f327e654c6
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2022-03-14 13:21:04 +1100

    Update aff_resnet.py

--- models/aff_resnet.py
+++ models/aff_resnet.py
@@ -249,6 +249,15 @@ def resnet18(fuse_type='DAF', **kwargs):
     """
     return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], fuse_type, **kwargs)
 
+def affresnet18(fuse_type='AFF', **kwargs):
+    r"""ResNet-18 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], fuse_type, **kwargs)
+
 
 def resnet34(fuse_type='DAF', **kwargs):
     r"""ResNet-34 model from
