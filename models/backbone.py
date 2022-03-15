40abdcf706414d1e58bfece9eea7ce25b562cb04
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2022-03-15 13:46:58 +1100

    Update backbone.py

--- models/backbone.py
+++ models/backbone.py
@@ -14,7 +14,7 @@ from typing import Dict, List
 from util.misc import NestedTensor, is_main_process
 
 from .position_encoding import build_position_encoding
-from .aff_resnet import resnet18
+from .aff_resnet import resnet50
 
 
 class FrozenBatchNorm2d(torch.nn.Module):
